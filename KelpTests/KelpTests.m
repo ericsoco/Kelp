361ff512fe73303cd8e46df1e343c1fce82ab4cb
Eric Socolofsky <eric@transmote.com>
2014-03-20 21:17:46 -0700

    Initial Commit

---
+++ KelpTests/KelpTests.m
@@ -0,0 +1,34 @@
+//
+//  KelpTests.m
+//  KelpTests
+//
+//  Created by Eric Socolofsky on 3/20/14.
+//  Copyright (c) 2014 Eric Socolofsky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KelpTests : XCTestCase
+
+@end
+
+@implementation KelpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
